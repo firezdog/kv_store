5b8097a6282835614ee4188f25b45f6e51dde58e
Alex Leibowitz <alex.leibowitz@zoro.com>
2024-06-26 10:41:17 -0500

    finish basic structure for insert

--- python/db.py
+++ python/db.py
@@ -9,12 +9,13 @@ class Database:
     _nhash: int
     _PTR_SIZE = 7       # the size (in chars) of a file offset pointer
     _DB_HASHOFF = 7     # hash table starts after the free list pointer
-    _DB_IDX_RECORD_START: int    # offset for index records portion of index file, used for locking
+    _DB_IDX_RECORDS_START: int    # offset for index records portion of index file, used for locking
+    _IDXLEN_SIZE = 4
 
     def __init__(self, name: str, nhash=256, overwrite=False):
         self._nhash = nhash
         # free list size + hash list size + newline
-        self._DB_IDX_RECORD_START = nhash * self._PTR_SIZE + self._DB_HASHOFF + 1
+        self._DB_IDX_RECORDS_START = nhash * self._PTR_SIZE + self._DB_HASHOFF + 1
 
         idx_file = f'{name}.idx'
         dat_file = f'{name}.dat'
@@ -26,8 +27,8 @@ class Database:
             self._dat_fd = open(dat_file, 'a+')
         else:
             # create new file and initialize hash line / free list
-            self._idx_fd = open(idx_file, 'w')
-            self._dat_fd = open(dat_file, 'w')
+            self._idx_fd = open(idx_file, 'w+')
+            self._dat_fd = open(dat_file, 'w+')
 
             # lock while we initialize the first line of the index file -- exclusive as no operations can proceed before init
             flock(self._idx_fd, LOCK_EX)
@@ -38,20 +39,28 @@ class Database:
             flock(self._idx_fd, LOCK_UN)
 
     def insert(self, key: str, value: str):
-        # get position of datafile end, then append data to data file (w/ exclusive lock)
-        flock(self._dat_fd, LOCK_EX)
-        data_file_end = self._dat_fd.seek(0, 2)
-        written = self._dat_fd.write(f'{value}\n')
-        flock(self._dat_fd, LOCK_UN)
+        # get position of datafile end, then append data to data file
+        data_offset = str(self._dat_fd.seek(0, 2))
+        data_length = str(self._dat_fd.write(f'{value}\n'))
+        # index record is "record_length:key:data_offset:data_length\n"
+        index_length = len(key) + 1 + len(data_offset) + 1 + len(data_length) + 1
 
         # determine where in the hash table the key goes
         key_hash = hash(key) % self._nhash
         hash_offset = key_hash * self._PTR_SIZE + self._DB_HASHOFF
 
-        # 1. write-lock hash table entry for this key -- read current head of hash chain
-        # 2. write index record with write-lock of records portion of file as new head (points to old)
+        # read current head of hash chain
+        self._idx_fd.seek(hash_offset)
+        ptr_value = int(self._idx_fd.read(self._PTR_SIZE))
+
+        # 2. write index record as new head (points to old)
+        index_record = f'{ptr_value:>{f"{self._PTR_SIZE}"}}{index_length:>{f"{self._IDXLEN_SIZE}"}}{key}:{data_offset}:{data_length}\n'
+        record_offset = f'{self._idx_fd.seek(0, 2):>{f"{self._PTR_SIZE}"}}'
+        self._idx_fd.write(index_record)
+
         # 3. update hash table entry to point to new index record
-        # 4. un-write-lock hash table entry for this key
+        self._idx_fd.seek(hash_offset)
+        self._idx_fd.write(record_offset)
 
     def __del__(self):
         self._idx_fd.close()
