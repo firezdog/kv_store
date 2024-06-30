709e32d96d5a632fc755f92592cd45d4292bb557
Alex Leibowitz <alex.leibowitz@zoro.com>
2024-06-30 14:30:45 -0700

    python version: add fetch

--- python/db.py
+++ python/db.py
@@ -39,6 +39,8 @@ class Database:
             flock(self._idx_fd, LOCK_UN)
 
     def insert(self, key: str, value: str):
+        # TODO: note this flow assumes the key is not present!
+        # TODO: locking
         # get position of datafile end, then append data to data file
         data_offset = str(self._dat_fd.seek(0, 2))
         data_length = str(self._dat_fd.write(f'{value}\n'))
@@ -46,8 +48,7 @@ class Database:
         index_length = len(key) + 1 + len(data_offset) + 1 + len(data_length) + 1
 
         # determine where in the hash table the key goes
-        key_hash = hash(key) % self._nhash
-        hash_offset = key_hash * self._PTR_SIZE + self._DB_HASHOFF
+        hash_offset = self._get_hash_offset(key=key)
 
         # read current head of hash chain
         self._idx_fd.seek(hash_offset)
@@ -62,6 +63,41 @@ class Database:
         self._idx_fd.seek(hash_offset)
         self._idx_fd.write(record_offset)
 
+    def fetch(self, target: str) -> str:
+        # TODO: locking
+        # 1. determine the hash index offset for the key
+        hash_offset = self._get_hash_offset(target)
+
+        # 2. read through each entry in the hash chain until the key is found
+        self._idx_fd.seek(hash_offset)
+        ptr_value = int(self._idx_fd.read(self._PTR_SIZE))
+
+        key, data_offset, data_size = '', 0, 0
+        while ptr_value != 0:
+            self._idx_fd.seek(ptr_value)
+            next_ptr_value = int(self._idx_fd.read(self._PTR_SIZE))
+            entry_size = int(self._idx_fd.read(self._IDXLEN_SIZE))
+            entry = self._idx_fd.read(entry_size)
+            key, data_offset, data_size = entry.strip().split(':')
+            if key == target:
+                # 3a. using the index entry for the key, read its value from the data file
+                self._dat_fd.seek(int(data_offset))
+                data = self._dat_fd.read(int(data_size)).strip()
+                return data
+
+            ptr_value = next_ptr_value
+
+        # 3b. if the key was not found, raise an error
+        raise ValueError('key not in database')
+
+    def _get_hash_offset(self, key: str) -> int:
+        # key_hash = hash(key) % self._nhash
+        # TODO: for testing -- remove
+        key_hash = 5
+        hash_offset = key_hash * self._PTR_SIZE + self._DB_HASHOFF
+
+        return hash_offset
+
     def __del__(self):
         self._idx_fd.close()
         self._dat_fd.close()
@@ -69,6 +105,15 @@ class Database:
 
 if __name__ == '__main__':
     db = Database('example', overwrite=True)
-    db.insert('a', '1')
-    db.insert('b', '2')
-    db.insert('c', '3')
+    db.insert('a', 'Anniston')
+    db.insert('b', 'Bob')
+    db.insert('c', 'Cary')
+    print(f'a:{db.fetch('a')}')
+    print(f'b:{db.fetch('b')}')
+    print(f'c:{db.fetch('c')}')
+    try:
+        db.fetch('d')
+    except ValueError:
+        print('did not find `d` in database -- will insert and try again')
+        db.insert('d', 'now here')
+        print(f'd:{db.fetch('d')}')
